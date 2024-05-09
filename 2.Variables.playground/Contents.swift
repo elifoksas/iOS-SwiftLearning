57f9546fc01886fe8d6a6b2cbdf13abd018d0868
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-05-09 16:03:49 +0300

    firstapp and variables

---
+++ 2.Variables.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+var ismim = "Elif"
+var soyismim = "Oban"
+
+print(ismim)
+
+ismim = "elif"
+
+//yorum satırı
+
+var userName = "test"
+
+var internettenGelenEnYeniVeri = 90 //camelCase
+var internetten_gelen_en_yeni_veri = 40 //snake_case
+
+
+//Integer
+let myNumber = 50 //sabit
+print(myNumber)
+//myNumber = 67 yapamıyoruz çünkü sabit
+
+myNumber.isMultiple(of: 2)
+
+
+//Double
+let pi = 3.14
+
+
+//Boolean
+var benimBooleanim = true
+benimBooleanim = false
+
+
+let benimSayim : Int16 = 50
+//benimSayim = 500000000000 -> overflow
+
+
+//defining , tanımlama
+let myString : String
+
+//initialization , değer atama
+myString = "Elif"
+
+
+
+var myNewNumber : Int
+myNewNumber = 20
+let benimStringNumaram : String
+benimStringNumaram = String(myNewNumber)
+
+
+
+
